db30e45a4a1f1d828faecc340a62acbf6a4a2af8
xuyucai <xuyucai@meideng.net>
2023-08-13 00:55:53 +0800

    feat(schedule): Support config concurrency.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub mod utils;
 use std::{
     collections::HashMap,
     sync::{atomic::AtomicBool, Arc},
+    time::Duration,
 };
 
 use contants::task::TaskStatus;
@@ -10,8 +11,9 @@ use serde_json::Value;
 pub mod contants;
 use handler::{Handler, HandlerFn, HandlerMap};
 pub mod handler;
-use log::{info, error};
+use log::{debug, error, info};
 use redis::AsyncCommands;
+use tokio::sync::{Semaphore, TryAcquireError};
 
 pub mod task;
 
@@ -20,6 +22,7 @@ pub struct SimpleTaskApp {
     pub tasks: HandlerMap,
     redis_client: redis::Client,
     should_stop: AtomicBool,
+    available_workers: Arc<Semaphore>,
 }
 
 const WAITING_TASK_ID_QUEUE: &str = "simple_task:waiting_task_id";
@@ -27,11 +30,12 @@ const PROCESSING_TASK_ID_QUEUE: &str = "simple_task:processing_task_id";
 const TASK_TABLE: &str = "simple_task:task";
 
 impl SimpleTaskApp {
-    pub fn new(redis_client: redis::Client) -> Self {
+    pub fn new(redis_client: redis::Client, concurrency: usize) -> Self {
         Self {
             tasks: HandlerMap::new(),
             redis_client,
             should_stop: AtomicBool::new(false),
+            available_workers: Arc::new(Semaphore::new(concurrency as usize)),
         }
     }
 
@@ -81,7 +85,7 @@ impl SimpleTaskApp {
     }
 
     pub async fn run_task(
-        self: Arc<Self>,
+        self: &Arc<Self>,
         task_id: &str,
         handler: &Arc<Handler>,
         input: Value,
@@ -89,8 +93,16 @@ impl SimpleTaskApp {
         let mut redis_conn = self.redis_client.get_async_connection().await?;
         let f = &handler.func;
         let task_id_key = join_key!(TASK_TABLE, task_id);
+        info!(
+            "Task {}, handler {}, task handler start.",
+            &task_id, &handler.name
+        );
         match f(input).await {
             Ok(result) => {
+                info!(
+                    "Task {}, handler {}, task handler done",
+                    &task_id, &handler.name
+                );
                 redis_conn
                     .hset_multiple(
                         task_id_key,
@@ -106,7 +118,10 @@ impl SimpleTaskApp {
                 redis_conn.expire(task_id_key, 3600).await?;
             }
             Err(e) => {
-                error!("Task {} failed, handler {}, error {}", &task_id, &handler.name, e);
+                error!(
+                    "Task {}, handler {}, task handler failed {}",
+                    &task_id, &handler.name, e
+                );
                 redis_conn
                     .hset_multiple(
                         task_id_key,
@@ -138,7 +153,14 @@ impl SimpleTaskApp {
             info!("  {}", name);
         }
         let conn_info = self.redis_client.get_connection_info();
-        info!("Broker & result backend: redis://{}/{}", conn_info.addr, conn_info.redis.db);
+        info!(
+            "Broker & result backend: redis://{}/{}",
+            conn_info.addr, conn_info.redis.db
+        );
+        info!(
+            "Concurrency: {}",
+            self.available_workers.available_permits()
+        );
 
         info!("Press CTRL+C to quit");
     }
@@ -151,6 +173,21 @@ impl SimpleTaskApp {
         let mut conn = _self.redis_client.get_async_connection().await?;
 
         loop {
+            let p = loop {
+                match _self.available_workers.clone().try_acquire_owned() {
+                    Ok(p) => {
+                        break p;
+                    }
+                    Err(e) => match e {
+                        TryAcquireError::NoPermits => {
+                            tokio::time::sleep(Duration::from_millis(100)).await;
+                        }
+                        TryAcquireError::Closed => {
+                            anyhow::bail!("Semaphore closed");
+                        }
+                    },
+                }
+            };
             if _self.should_stop() {
                 break;
             }
@@ -196,12 +233,24 @@ impl SimpleTaskApp {
                 .get(&handler_name)
                 .ok_or(anyhow::anyhow!("Handler {} not found", handler_name))?
                 .clone();
+
+            info!("Task {} accepted, handler {}", &task_id, handler_name);
+
             let task_id = task_id.clone();
             let _self = _self.clone();
             tokio::spawn(async move {
+                let _p = p;
                 if let Err(e) = _self.run_task(&task_id, &handler, input).await {
-                    error!("Internal error when running task {}, handler {}, error {}", &task_id, handler_name, e);
+                    error!(
+                        "Internal error when running task {}, handler {}, error {}",
+                        &task_id, handler_name, e
+                    );
                 }
+
+                debug!(
+                    "Task done. Task id {}, handler {}, Releasing SemaphorePermit.",
+                    &task_id, handler_name
+                );
             });
         }
         Ok(())
